c02aa63f3e4be080b0aa7448708f0406bcfbb698
orkun pınarkaya <orkunpinarkaya1@gmail.com>
2021-04-24 03:09:25 +0300

    fix
    fix

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNetDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ProductDal _productdal = new ProductDal();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            TabloGetir();
+        }
+        private void TabloGetir()
+        {
+            dgwProducts.DataSource = _productdal.GelAll();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _productdal.Add(new Product
+            {
+                Name = tbxName.Text,
+                UnitPrice = Convert.ToInt32(tbxUnitPrice.Text),
+                StockAmount = Convert.ToInt32(tbxStockAmount.Text)
+
+            });
+            MessageBox.Show("product added");
+            TabloGetir();
+        }
+
+        private void dgwProducts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgwProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbxNameUpdate.Text = dgwProducts.CurrentRow.Cells[1].Value.ToString();
+            tblUnitPriceUpdate.Text = dgwProducts.CurrentRow.Cells[2].Value.ToString();
+            tbxStockAmountUpdate.Text = dgwProducts.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Product product = new Product
+            {
+                Id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value),
+                Name = tbxNameUpdate.Text,
+                UnitPrice= Convert.ToDecimal(tblUnitPriceUpdate.Text),
+                StockAmount = Convert.ToInt32(tbxStockAmountUpdate.Text)
+
+            };
+            _productdal.Update(product);
+            TabloGetir();
+            MessageBox.Show("Updated");
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value);
+            _productdal.Delete(id);
+            MessageBox.Show("deleted");
+            TabloGetir();
+        }
+    }
+}
---
+++ ProductDal.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDemo
+{
+    public class ProductDal
+    {
+        SqlConnection _connection = new SqlConnection(@"server=(localdb)\MSSQLLocalDB;initial catalog=ETrade;integrated security=true");
+        public  List<Product> GelAll()
+        {
+            ConnectionControl();
+          
+
+            SqlCommand command = new SqlCommand("select * from Products",_connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Product> products = new List<Product>();
+
+            while (reader.Read())
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    StockAmount=Convert.ToInt32(reader["StockAmount"]),
+                    UnitPrice=Convert.ToDecimal(reader["UnitPrice"])
+                };
+                products.Add(product);
+            }
+            reader.Close();
+            _connection.Close();
+            return products;
+          
+        }
+        //public DataTable GelAll2()
+        //{
+        //    SqlConnection connection = new SqlConnection(@"server=(localdb)\MSSQLLocalDB;initial catalog=ETrade;integrated security=true");
+        //    if (connection.State == ConnectionState.Closed)
+        //    {
+        //        connection.Open();
+        //    }
+
+        //    SqlCommand command = new SqlCommand("select * from Products", connection);
+
+        //    SqlDataReader reader = command.ExecuteReader();
+
+        //    DataTable datatable = new DataTable();
+        //    datatable.Load(reader);
+        //    reader.Close();
+        //    connection.Close();
+        //    return datatable;
+
+        //}
+        private void ConnectionControl()
+        {
+            if (_connection.State == ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+        public void Add(Product product)
+        {
+            ConnectionControl();
+            SqlCommand _command = new SqlCommand("insert into Products values(@name,@unitPrice,@stockAmount)",_connection);
+            _command.Parameters.AddWithValue("@name", product.Name);
+            _command.Parameters.AddWithValue("@unitPrice", product.UnitPrice);
+            _command.Parameters.AddWithValue("@stockAmount", product.StockAmount);
+            _command.ExecuteNonQuery();
+
+            _connection.Close();
+
+        }
+        public void Update(Product product)
+        {
+            ConnectionControl();
+            SqlCommand _command = new SqlCommand("update Products set Name=@name,UnitPrice=@unitPrice,StockAmount=@stockAmount where Id=@id", _connection);
+            _command.Parameters.AddWithValue("@name", product.Name);
+            _command.Parameters.AddWithValue("@unitPrice", product.UnitPrice);
+            _command.Parameters.AddWithValue("@stockAmount", product.StockAmount);
+            _command.Parameters.AddWithValue("@id", product.Id);
+            _command.ExecuteNonQuery();
+
+            _connection.Close();
+
+        }
+        public void Delete(int id)
+        {
+            ConnectionControl();
+            SqlCommand _command = new SqlCommand("delete from products where Id=@id", _connection);
+           
+            _command.Parameters.AddWithValue("@id", id);
+            _command.ExecuteNonQuery();
+
+            _connection.Close();
+
+        }
+    }
+}
